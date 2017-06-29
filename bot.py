293f2de2c559ba8c1bbd959ee6a4ee1b9e88a6b9
Alec DiFederico <alecdifederico@gmail.com>
2017-06-28 23:59:52 -0400

    updated PRAW

---
+++ bot.py
@@ -0,0 +1,74 @@
+import praw
+import time
+import random
+from facts import RANDOM_FACTS
+
+num_of_comments = 50
+num_of_iterations = 10
+
+
+"""
+replies to reddit comments about salad dressings
+"""
+
+# create the reddit object
+# reddit sees who accesses this info so be descriptive
+r = praw.Reddit(client_id='xxxxxxxxxxxxxx',
+                client_secret='xxxxxxxxxxxxxxxxxxxxx',
+                password='password',
+                user_agent="bot that does blah blah blah",
+                username='username')
+
+print("Logging in...")
+
+
+# looking for the words/phrases about salad dressings
+words_to_match = ['vinaigrette',  'vinaigrettes', 'salad dressings', 'salad dressing',
+                  'dressing', 'dressings', 'salad-dressing', 'salad-dressings', 'salad',
+                  'lettuce']
+
+
+# the comments that the bot has already reached
+with open("black_list.txt", 'r') as f:
+    black_list = [line.rstrip('\n') for line in f]
+
+
+def run_bot():
+    flag = False
+    print("Grabbing subreddit...")
+    # find the subreddit that you want to scan
+    subreddit = r.subreddit("salads")
+    print("Grabbing comments...")
+    # pull out comments from the subreddit, pulling 25 comments at a time
+    comments = subreddit.comments(limit=num_of_comments)
+
+    # list of comment objects in comments
+    for comment in comments:
+        # get the text in the comment and convert it all to lowercase
+        comment_text = comment.body.lower()
+        # any of the words in comment_text match any of the words in words_to_match?
+        is_match = any(string in comment_text for string in words_to_match)
+
+        # if comment is not in the black_list and it's a match
+        if comment.id not in black_list and is_match and not flag:
+            print("Match found! Comment ID: " + comment.id)
+            # each comment has its own id, append the id to the black_list
+            black_list.append(comment.id)
+
+            # place the comment id into the black_list file
+            with open("black_list.txt", 'w') as f:
+                for s in black_list:
+                    f.write(s + '\n')
+
+            # the reply to the comment with a random fact
+            comment.reply("A fact about salad dressings: " + random.choice(RANDOM_FACTS))
+            print("Reply successful!")
+            flag = True
+
+
+    print("Comment loop finished.")
+
+# run the bot a specified amount of times
+for i in range(0, num_of_iterations - 1):
+    run_bot()
+    time.sleep(5)
---
+++ facts.py
@@ -0,0 +1,59 @@
+RANDOM_FACTS = [
+        """The practice of using a vinaigrette goes back to ancient times of putting
+        vinegar and oil on vegetables. Vinaigrettes have stood the test of time!""",
+
+        """"The word 'vinaigrette,' which originated as a diminutive form of French
+        vinaigre (vinegar), was first used in English as long ago as 1699
+        (John Evely mentioned it in his book on salads, Acetaria) but it did
+        not really become established until the end of the nineteenth century!""",
+
+        """Although there is no totally fixed definition, a vinaigrette is usually
+        considered to be A cold sauce made from a mixture of vinegar oil, pepper,
+        and salt, to which various flavourings may be added!""",
+
+        """Vinaigrette is considered to be a typically French sauce and is often
+         called 'French dressing' in Britain!""",
+
+        """It was a French emigre, Chevalier d'Albingac, who started the
+         fashion in London high society for salads dressed in the fashion of a
+         vinaigrette!""",
+
+        """The most common additions to a vinaigrette are mustard, shallots, onions,
+        basil, dill, or parsley!""",
+
+        """The most common oils used in vinaigrettes are olive oil and
+        canola oil!""",
+
+        """In general, a vinaigrette consists of 3 parts of oil to 1 part of
+        vinegar whisked into an emulsion!""",
+
+        """Balsamic vinaigrette is made by adding a small amount of balsamic
+        vinegar to a simple vinaigrette of olive oil and wine vinegar!""",
+
+        """The Babylonians used oil and vinegar for dressing greens nearly
+        2,000 years ago!""",
+
+        """England’s King Henry IV's favorite salad was a tossed mixture of
+        new potatoes (boiled and diced), sardines and herb dressing! """,
+
+        """Mary, Queen of Scots, preferred boiled celery root diced and tossed
+         with lettuce, creamy mustard dressing, truffles, chervil and
+         hard-cooked egg slices!""",
+
+        """Many of the major brands of salad dressings available today were on
+         the market as early as the 1920’s!""",
+
+        """In 1896, Joe Marzetti opened a restaurant in Columbus, OH and
+         began to serve his customers a variety of dressings developed from
+          old country recipes. He began packaging his dressings to sell to
+           restaurant customers in 1919!""",
+
+        """Russian dressing got its name because the earliest versions of the
+         mixture of mayonnaise, pimientos, chives, ketchup, and spices included
+          a distinctly Russian ingredient: caviar!""",
+
+        """The most important rule in preparing a vinaigrette is to dissolve
+         the salt in vinegar first and then add the oil. Blending all ingredients
+          at once will always end up oily, never tart, nor refreshing! """
+    ]
+
